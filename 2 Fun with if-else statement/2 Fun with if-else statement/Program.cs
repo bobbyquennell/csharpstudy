10574cd2031dfc2ee42b88dbd3ae25417dfa1b58
WANG <bobbyquennell@gmail.com>
2013-05-26 08:30:32 +0800

    chapter2: Fun with if/else statemens

---
+++ 2 Fun with if-else statement/2 Fun with if-else statement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace _2_Fun_with_if_else_statement
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
