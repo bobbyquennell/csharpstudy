d060005515793c5b8997f9c3c7dd9e1149f71b78
WANG <bobbyquennell@gmail.com>
2013-05-28 22:47:12 +0800

    chapter 3: pool puzzle

---
+++ 2 pool puzzle/2 pool puzzle/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _2_pool_puzzle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            String Poem = "";
+            while (x < 4)
+            {
+                Poem = Poem + "a";
+                if (x < 1)
+                {
+                    Poem = Poem + " ";
+                }
+                Poem = Poem + "n";
+                if (x < 1)
+                {
+
+                    Poem = Poem + "oise ";
+                    x = x - 1;
+
+                }
+                if (x == 1)
+                {
+                    Poem = Poem + "noys ";
+
+                }
+                if (x > 1)
+                {
+                    //
+                    Poem = Poem + " oyster";
+                }
+                x = x + 2;
+
+            }
+            MessageBox.Show(Poem);
+        }
+    }
+}
---
+++ chapter3 pool puzzle/chapter3 pool puzzle/Class for pool puzzle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chapter3_pool_puzzle
+{
+    class Echo
+    {
+        public int count = 0;
+        public string Hello() { 
+          return "helloooo...";
+        }
+    }
+}
