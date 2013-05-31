b54a10a70d81000d41f7d040a5db37d2cb7aad2f
WANG <bobbyquennell@gmail.com>
2013-05-31 22:05:44 +0800

    chapter4: swap

---
+++ 4_Swap/4_Swap/Elephant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _4_Swap
+{
+    class Elephant
+    {
+        public int EarSize;
+        public string Name;
+        public void WhoAmI()
+        {
+            MessageBox.Show("My ears are " + EarSize + " inches tall.", Name + " says...");
+        }
+    }
+}
---
+++ 4_Swap/4_Swap/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _4_Swap
+{
+    public partial class Form1 : Form
+    {
+        Elephant Lloyd, Lucinda;
+        public Form1()
+        {
+            InitializeComponent();
+            Lloyd = new Elephant() { Name = "Lloyd", EarSize = 33};
+            Lucinda = new Elephant() { Name = "Lucinda", EarSize = 40};
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Lloyd.WhoAmI();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Lucinda.WhoAmI();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Elephant TempRef;
+            TempRef = Lloyd;
+            Lloyd = Lucinda;
+            Lucinda = TempRef;
+            MessageBox.Show("Objects swapped");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Lloyd = Lucinda;
+            Lloyd.EarSize = 4321;
+            Lloyd.WhoAmI();
+        }
+
+    }
+}
