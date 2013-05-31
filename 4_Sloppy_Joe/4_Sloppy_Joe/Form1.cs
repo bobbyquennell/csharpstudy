fa686b418adbc6f206d6af36365b5c707643cf25
WANG <bobbyquennell@gmail.com>
2013-05-31 23:35:52 +0800

    chapter4 Sloppy Joe

---
+++ 4_Sloppy_Joe/4_Sloppy_Joe/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _4_Sloppy_Joe
+{
+    public partial class Form1 : Form
+    {
+        MenuMaker myMenu = new MenuMaker() { Randomrizer = new Random()};
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = myMenu.GetMenumItem();
+            label2.Text = myMenu.GetMenumItem();
+            label3.Text = myMenu.GetMenumItem();
+            label4.Text = myMenu.GetMenumItem();
+            label5.Text = myMenu.GetMenumItem();
+            label6.Text = myMenu.GetMenumItem();
+
+        }
+        
+    }
+}
---
+++ 4_Sloppy_Joe/4_Sloppy_Joe/MenuMaker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _4_Sloppy_Joe
+{
+    class MenuMaker
+    {
+        public Random Randomrizer;
+        public string[] Meats = {"Roast beef", "Salami", "Turkey", "Ham", "Pastrami"};
+        public string[] Condiments = {"yellow mastard", "brown mustard", 
+                                     "honey mustard", "mayo", "relish", "french dressing" };
+        public string[] Breads = {"rye", "white", "wheat", "pumpernickel",
+                                 "italian bread", "a roll"};
+        public string GetMenumItem()
+        {
+            string meat = Meats[Randomrizer.Next(Meats.Length)];
+            string condiment = Condiments[Randomrizer.Next(Condiments.Length)];
+            string bread = Breads[Randomrizer.Next(Breads.Length)];
+            return meat + " with " + condiment + " on " + bread;
+ 
+        }
+    }
+}
