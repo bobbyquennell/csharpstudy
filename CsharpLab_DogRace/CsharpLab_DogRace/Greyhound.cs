6fb74f2ac9533e0dbc2198ab90067a4200a24844
Bobby <bobbyquennell@gmail.com>
2013-06-05 13:04:25 +0800

    CsharpLab:DogRace

---
+++ CsharpLab_DogRace/CsharpLab_DogRace/Greyhound.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace CsharpLab_DogRace
+{
+    class Greyhound
+    {
+        public int StartinPositon;
+        public int RacetrackLength;
+        public PictureBox MyPictureBox = null;
+        public int Location = 0;
+        public Random Randomizer;
+
+    }
+}
