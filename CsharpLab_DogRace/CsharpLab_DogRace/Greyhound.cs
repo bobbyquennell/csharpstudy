4946c44421e48dec8e0565828f551a44d5d62e8a
Bobby <bobbyquennell@gmail.com>
2013-06-05 15:54:12 +0800

    update Greyhound.cs

--- CsharpLab_DogRace/CsharpLab_DogRace/Greyhound.cs
+++ CsharpLab_DogRace/CsharpLab_DogRace/Greyhound.cs
@@ -7,11 +7,25 @@ namespace CsharpLab_DogRace
 {
     class Greyhound
     {
-        public int StartinPositon;
-        public int RacetrackLength;
-        public PictureBox MyPictureBox = null;
-        public int Location = 0;
-        public Random Randomizer;
+        public int StartinPositon;// where my picturebox starts
+        public int RacetrackLength;// how long the racetrack is 
+        public PictureBox MyPictureBox = null;// My picturebox object
+        public int Location = 0;// my location on the racetrack
+        public Random Randomizer;// an instance of Random
+        // Only need one instance of  Random-- each Greyhound's Randomizer reference should
+        //point to the same Random object.
+        public bool Run()
+        {
+            //move forward either 1,2,3 or 4 spaces at random
+            Location += Randomizer.Next(1, 4);
+            //Update the position of my PictureBox on the form
+            // Return true if I won the race
+        }
+        public void TakeStartingPosition()
+        {
+            // Reset my location to the start line
+            Location = 0;
+        }
 
     }
 }
