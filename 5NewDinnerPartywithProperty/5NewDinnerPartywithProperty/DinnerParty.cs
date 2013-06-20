80239c7e3735dd73e0d5a2ee6307d545449a8076
WANG <bobbyquennell@gmail.com>
2013-06-20 09:13:11 +0800

    add new dinnerparty class with property

---
+++ 5NewDinnerPartywithProperty/5NewDinnerPartywithProperty/DinnerParty.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace _5NewDinnerPartywithProperty
+{
+    class DinnerParty
+    {
+        private const int CostPerPeople = 25;
+        private int numOfPeople;
+        public int NumOfPeople {
+            get { return numOfPeople; }
+            set { numOfPeople = value; }
+            //tbd: calculate cost of decoration here }
+        }
+        private bool fancyDecoration;
+        public DinnerParty(int numOfPeople, bool fancyDecoration, bool healtyOption) 
+        {
+
+        }
+
+    }
+}
