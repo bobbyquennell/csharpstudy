2b3d622263413efb8ceaad8ae21c9f3e15db2f42
WANG <bobbyquennell@gmail.com>
2013-06-21 09:16:05 +0800

    add birthdayparty

---
+++ 6DinnerPlanner2.0/6DinnerPlanner2.0/BirthdayParty.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _6DinnerPlanner2._0
+{
+    class BirthdayParty
+    {
+        private const decimal costPerPerson = 25M;
+        private const decimal CakePrice8inches = 40M;
+        private const decimal CakePrice16inches = 75M;
+        private const decimal WritingPricePerCharacter = 0.25M;
+        private int MaxCharacters;
+        private int numOfPeople;
+        public int NumOfPeople
+        {
+            get { return numOfPeople; }
+            set { numOfPeople = value;
+                  CalculateCostOfDecorations();
+                 }
+        }
+        private bool isFancyDecoration;
+        public bool IsFancyDecoration 
+        {
+            get
+            {
+                return isFancyDecoration;
+            }
+            set 
+            { 
+                isFancyDecoration = value;
+                CalculateCostOfDecorations();
+
+            } 
+        }
+        private int cakeSize;
+        private string cakeWriting;
+        private decimal costOfDecoration;
+        public BirthdayParty(int numOfPeople,bool isFancyDeocration, string cakeWriting)
+        {
+            NumOfPeople = numOfPeople;
+            cakeChoose(NumOfPeople, cakeWriting);
+            IsFancyDecoration = isFancyDecoration;
+
+            CalculateCostOfDecorations();
+        }
+        private void cakeChoose(int personNum,string cakeWriting)
+        {
+            if (personNum <= 4)
+            {
+                cakeSize = 8;
+                MaxCharacters = 16;
+            }
+            else
+            {
+                cakeSize = 16;
+                MaxCharacters = 40;
+            }
+            if (cakeWriting.Length > MaxCharacters)
+            {
+                MessageBox.Show("Error", "the maximum characters is" + MaxCharacters.ToString());
+            }
+            else
+            {
+                this.cakeWriting = cakeWriting;
+            }
+        }
+        private void CalculateCostOfDecorations()
+        {
+            if (IsFancyDecoration)
+            {
+                costOfDecoration = NumOfPeople * 15M + 50M;
+            }
+            else
+            {
+                costOfDecoration = NumOfPeople * 7.5M + 30;
+            }
+        }
+        public int CalculateCost()
+        {
+            decimal TotalCost;
+            decimal CakePrice;
+            if ()
+            {
+                
+            }
+
+            return 1;
+        }
+
+    }
+}
---
+++ 6DinnerPlanner2.0/6DinnerPlanner2.0/DinnerParty.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _6DinnerPlanner2._0
+{
+    class DinnerParty
+    {
+        private const decimal CostPerPeople = 25;
+        private int numOfPeople;
+        public int NumOfPeople {
+            get 
+            { 
+                return numOfPeople; 
+            }
+            set 
+            { 
+                numOfPeople = value;
+                calculateDecoration();
+            }
+        }
+        private bool fancyDecoration;
+        public bool FancyDecoration
+        {
+            get
+            {
+                return fancyDecoration;
+            }
+            set 
+            {
+                fancyDecoration = value;
+                calculateDecoration();
+            }
+        }
+        private decimal costOfDecoration;
+        public bool HealthyOption { get; set; }
+        public DinnerParty(int numOfPeople, bool fancyDecoration, bool healtyOption) 
+        {
+            NumOfPeople = numOfPeople;
+            this.fancyDecoration = fancyDecoration;
+            HealthyOption = healtyOption;
+            calculateDecoration();
+            //constructor
+        }
+        private void calculateDecoration()
+        {
+            if (fancyDecoration)
+            {
+                costOfDecoration = (decimal)NumOfPeople * 15m + 50m;
+            }
+            else
+            {
+                costOfDecoration = (decimal)NumOfPeople * 7.5m + 30m;
+            }
+        }
+        public decimal GetPartyCost()
+        {
+            decimal TotalCost = 0m;
+            if (HealthyOption)
+	        {
+                TotalCost = costOfDecoration + (decimal)NumOfPeople * (CostPerPeople + 5m) * 0.95m;
+		 
+	        }
+            else
+            {
+                TotalCost = costOfDecoration + (decimal)NumOfPeople * (CostPerPeople + 20m);
+            }
+            return TotalCost;
+        }
+
+    }
+}
