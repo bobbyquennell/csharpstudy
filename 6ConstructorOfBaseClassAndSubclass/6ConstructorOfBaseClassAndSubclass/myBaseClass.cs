3347d0c0e926bfd16f3c9d1c7b1228876018d343
BobbyQuennell <bobbyquennell@gmail.com>
2013-06-23 22:15:35 +0800

    add 6ConstructorOfBaseClassAndSubclass

---
+++ 6ConstructorOfBaseClassAndSubclass/6ConstructorOfBaseClassAndSubclass/myBaseClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace _6ConstructorOfBaseClassAndSubclass
+{
+    class myBaseClass
+    {
+        public myBaseClass(string baseClassNeedsThis)
+        {
+            MessageBox.Show("This is the  base class: " + baseClassNeedsThis);
+        }
+    }
+}
