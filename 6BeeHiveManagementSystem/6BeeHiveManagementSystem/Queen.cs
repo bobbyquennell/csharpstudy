99a3f844bd5359c71b5904f522137241e23e43a3
BobbyQuennell <bobbyquennell@gmail.com>
2013-06-25 22:30:43 +0800

    continue coding on beehiveManagement system

---
+++ 6BeeHiveManagementSystem/6BeeHiveManagementSystem/Queen.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _6BeeHiveManagementSystem
+{
+    class Queen
+    {
+        private worker[] workers;
+        private int shiftsNumber;
+        public Queen(worker[] workers, int shiftsNumber)
+        {
+            //creat the queen's worker bees
+            this.workers = new worker[workers.Length];
+            this.workers = workers;
+            // initialize some private value;
+            //shiftsNumber ???????
+        }
+        public bool AssignWork(string JobToDo, int ShiftsNeeded)
+        {
+            // find a worker that is available for this Job.
+            for (int i = 0; i < workers.Length - 1; i++)
+            {
+                if (workers[i].DoThisJob(JobToDo, ShiftsNeeded))
+                {
+                    return true;
+                }
+                
+            }
+            //cannot find one available worker bee to do this job
+            return false;
+        }
+        public void WorkTheNextShift()
+        {
+            //maker all worker bees to work a shift
+            for (int i = 0; i < workers.Length - 1; i++)
+            {
+                workers[i].WorkOneShift();
+
+            }
+            //check worker bees' status
+            //make a shift's report.
+        }
+        private string getWorkerbeeStatus(worker theWorker)
+        {
+           if(string.IsNullOrEmpty(theWorker.CurrentJob))
+           {}  //output: worker #x is not working;
+           else
+	       {
+               switch (theWorker.ShiftsLeft)
+               {
+                  default:
+               }
+	       }
+
+        }
+    }
+}
--- 6BeeHiveManagementSystem/6BeeHiveManagementSystem/worker.cs
+++ 6BeeHiveManagementSystem/6BeeHiveManagementSystem/worker.cs
@@ -7,6 +7,55 @@ namespace _6BeeHiveManagementSystem
 {
     class worker
     {
-
+        private string[] jobICanDo;
+        public string CurrentJob { get; private set; }
+        public int ShiftsLeft { get; private set; }
+        private int shiftsToWork;
+        private int shiftsWorked;
+        public worker(string[] jobCanDo)
+        {
+            jobICanDo = new string[jobCanDo.Length];
+            jobICanDo = jobCanDo;
+            //for (int i = 0; i < jobCanDo.Length - 1; i++)
+            //{
+            //    jobICanDo[i] = jobCanDo[i];
+            //}
+            
+        }
+        public bool DoThisJob(string jobToDo, int shiftsToWork)
+        {
+            if (isJobICanDo(jobToDo))
+	        {
+                if (string.IsNullOrEmpty(CurrentJob))
+                {
+                    CurrentJob = jobToDo;
+                    this.shiftsToWork = shiftsToWork;
+                    return true;
+                }
+		 
+	        }
+            return false;
+        }
+        public void WorkOneShift(){
+            shiftsWorked++;
+            ShiftsLeft = shiftsToWork - shiftsWorked;
+            if (ShiftsLeft == 0)
+            {
+                shiftsToWork = 0;
+                shiftsWorked = 0;
+                CurrentJob = "";
+            }
+        }
+        private bool isJobICanDo(string job)
+        {
+            for (int i = 0; i < jobICanDo.Length - 1; i++)
+            {
+                if (jobICanDo[i] == job)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
