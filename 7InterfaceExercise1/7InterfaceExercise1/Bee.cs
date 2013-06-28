e1cea478f919c2a46235a55530228412882fc3a7
BobbyQuennell <bobbyquennell@gmail.com>
2013-06-28 09:18:43 +0800

    add 7 interface exercise 1: IstingPatrol

---
+++ 7InterfaceExercise1/7InterfaceExercise1/Bee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _7InterfaceExercise1
+{
+    class Bee:IStingPatrol
+    {
+        public int SharpenStinger(int Stingerlength)
+        { return Stingerlength; }
+        public bool LookForEnemies()
+        { return true; }
+        public int StingerLength 
+        { 
+            get; 
+            set; 
+        }
+        public int AlertLevel 
+        {
+            get
+            { return 1; }
+            //set; 
+        }
+
+    }
+}
