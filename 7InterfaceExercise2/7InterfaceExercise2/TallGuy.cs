d85a79c46c705dd38d41d42a532cb4436462598c
Bobby <bobbyquennell@gmail.com>
2013-06-28 13:32:13 +0800

    add 7 exercise2 interface Tallguy

---
+++ 7InterfaceExercise2/7InterfaceExercise2/TallGuy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _7InterfaceExercise2
+{
+    class TallGuy
+    {
+        public string Name;
+        public int Height;
+        public void TalkAboutYourself() {
+            MessageBox.Show("My name is: " + Name + " and I'm " 
+                + Height + " inches tall.");
+        }
+    }
+}
