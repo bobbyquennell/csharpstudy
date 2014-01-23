0fddc81c026679aae3881ce7745f391e68dce622
Bobby <bobbyquennell@gmail.com>
2014-01-23 17:52:24 +0800

    create chapter8 exercise 1

---
+++ 8Exercise1AClassHoldsAPlayingCard/8Exercise1AClassHoldsAPlayingCard/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _8Exercise1AClassHoldsAPlayingCard
+{
+    public partial class Form1 : Form
+    {
+        Card myCard;
+        Suit mySuit;
+        Value myValue;
+        public Form1()
+        {
+            InitializeComponent();
+   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
