90ee2b4955bd281e23df54ce04584de93e7b7fee
Bobby <bobbyquennell@gmail.com>
2014-01-26 15:37:11 +0800

    complete Chapter8 exercise 1

--- 8Exercise1AClassHoldsAPlayingCard/8Exercise1AClassHoldsAPlayingCard/Form1.cs
+++ 8Exercise1AClassHoldsAPlayingCard/8Exercise1AClassHoldsAPlayingCard/Form1.cs
@@ -11,18 +11,22 @@ namespace _8Exercise1AClassHoldsAPlayingCard
 {
     public partial class Form1 : Form
     {
-        Card myCard;
+        Random random = new Random();
         Suit mySuit;
         Value myValue;
+        Card myCard;
         public Form1()
         {
             InitializeComponent();
    
         }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
-
+            mySuit = (Suit)random.Next(4);
+            myValue = (Value)random.Next(1,14);
+            myCard = new Card(mySuit, myValue);
+            MessageBox.Show(myCard.Name);
         }
     }
 }
