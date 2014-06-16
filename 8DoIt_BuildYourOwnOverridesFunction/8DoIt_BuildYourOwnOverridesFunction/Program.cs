fe8b04b3017155853b48c0e42d9bcc6a0290cd35
BobbyQuennell <bobbyquennell@gmail.com>
2014-06-16 23:34:47 +0800

    add chapter8 do it (overrides function) 2014-Jun-16

---
+++ 8DoIt_BuildYourOwnOverridesFunction/8DoIt_BuildYourOwnOverridesFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _8DoIt_BuildYourOwnOverridesFunction
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press any key to continue.....");
+            Console.ReadKey();
+            Card myCard = new Card(Suit.Diamonds, Value.King);
+            bool isCardMatch = Card.DoesCardMatch(myCard.Suit, Suit.Diamonds);
+            Console.WriteLine(isCardMatch.ToString());
+            isCardMatch = Card.DoesCardMatch(myCard.Value, Value.Jack);
+            Console.WriteLine(isCardMatch.ToString());
+            Console.WriteLine("Press \"Enter\" to quit....");
+            Console.ReadLine();
+
+        }
+    }
+}
