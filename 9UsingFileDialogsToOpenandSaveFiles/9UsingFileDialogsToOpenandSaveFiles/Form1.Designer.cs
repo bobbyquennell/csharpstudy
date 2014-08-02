3f08511356b7d16b5064d6306c92c24cd2a61362
unknown <bobbyquennell@gmail.com>
2014-08-02 12:24:47 +0800

    add code: simple file editor in chapter 9

---
+++ 9UsingFileDialogsToOpenandSaveFiles/9UsingFileDialogsToOpenandSaveFiles/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace _9UsingFileDialogsToOpenandSaveFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Openbutton = new System.Windows.Forms.Button();
+            this.Savebutton = new System.Windows.Forms.Button();
+            this.TextEditorBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Openbutton
+            // 
+            this.Openbutton.Location = new System.Drawing.Point(292, 3);
+            this.Openbutton.Name = "Openbutton";
+            this.Openbutton.Size = new System.Drawing.Size(75, 30);
+            this.Openbutton.TabIndex = 0;
+            this.Openbutton.Text = "Open";
+            this.Openbutton.UseVisualStyleBackColor = true;
+            this.Openbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Savebutton
+            // 
+            this.Savebutton.Location = new System.Drawing.Point(373, 3);
+            this.Savebutton.Name = "Savebutton";
+            this.Savebutton.Size = new System.Drawing.Size(75, 30);
+            this.Savebutton.TabIndex = 1;
+            this.Savebutton.Text = "Save";
+            this.Savebutton.UseVisualStyleBackColor = true;
+            this.Savebutton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // TextEditorBox
+            // 
+            this.TextEditorBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TextEditorBox.Location = new System.Drawing.Point(3, 3);
+            this.TextEditorBox.Multiline = true;
+            this.TextEditorBox.Name = "TextEditorBox";
+            this.TextEditorBox.Size = new System.Drawing.Size(451, 221);
+            this.TextEditorBox.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.TextEditorBox, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 86.95652F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.04348F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(457, 262);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.Savebutton);
+            this.flowLayoutPanel1.Controls.Add(this.Openbutton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 230);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(451, 29);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 262);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Simple Text Editor";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Openbutton;
+        private System.Windows.Forms.Button Savebutton;
+        private System.Windows.Forms.TextBox TextEditorBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ 9UsingFileDialogsToOpenandSaveFiles/9UsingFileDialogsToOpenandSaveFiles/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace _9UsingFileDialogsToOpenandSaveFiles
+{
+    public partial class Form1 : Form
+    {
+        private string name;
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.InitialDirectory = "C:\\Users\\LogicPlatypus\\Downloads";
+            saveFileDialog1.Title = "change title text";
+            openFileDialog1.Filter = "Text Files(*.txt)|*.txt";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+                TextEditorBox.Clear();
+                name = openFileDialog1.FileName;
+                TextEditorBox.Text = File.ReadAllText(openFileDialog1.FileName);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if(result == System.Windows.Forms.DialogResult.OK)
+                File.WriteAllText(saveFileDialog1.FileName, TextEditorBox.Text);
+        }
+    }
+}
